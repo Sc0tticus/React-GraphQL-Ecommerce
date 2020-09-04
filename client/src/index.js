2dcec1616a8328e408c8f2014b98cc8be8a61486
Sc0tticus <ssinger303@gmail.com>
2020-09-04 12:10:01 -0600

    Deleted App.css code, index.css, and modified index.js. Added NavBar component

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './index.css';
+import 'gestalt/dist/gestalt.css';
 import App from './components/App';
+import Navbar from './components/Navbar';
 import registerServiceWorker from './registerServiceWorker';
 import Signin from './components/Signin';
 import Signup from './components/Signup';
@@ -10,12 +11,15 @@ import Checkout from './components/Checkout';
 
 const Root = () => (
   <Router>
+    <React.Fragment>
+    <Navbar />
     <Switch>
       <Route component={App} exact path="/" />
       <Route component={Signin} path="/signin" />
       <Route component={Signup} path="/signup" />
       <Route component={Checkout} path="/checkout" />
     </Switch>
+    </React.Fragment>
   </Router>
 )
 
